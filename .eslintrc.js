313070834cc372050290c2985f51f5f7a601fb74
Ablesons <huangzhendong1996@outlook.com>
2022-02-17 15:14:28 +0800

    初始导入

--- .eslintrc.js
+++ .eslintrc.js
@@ -30,7 +30,7 @@ module.exports = {
    * "error" -> 2 开启错误规则
    */
   rules: {
-// 不需要
+    // 不需要
     "space-before-function-paren": 0, // 函数定义时括号前面要不要有空格
     "eol-last": 0, // 文件以单一的换行符结束
     "no-extra-semi": 0, // 可以多余的冒号
@@ -39,7 +39,63 @@ module.exports = {
     "one-var": 0, // 连续声明
     "no-undef": 0, // 可以 有未定义的变量
     "no-console": process.env.NODE_ENV === 'production' ? 'warn' : 'off',
-    "no-unused-vars": 0, // 不能有声明后未被使用的变量或参数
+    '@typescript-eslint/ban-ts-ignore': 'off', // 禁止使用@ts-ignore来消除ESLint检查
+    '@typescript-eslint/explicit-function-return-type': 'off', // 在函数和类方法上需要显式的返回类型
+    '@typescript-eslint/no-explicit-any': 'off', // 禁止使用any类型
+    '@typescript-eslint/no-var-requires': 'off', // 除导入语句外，禁止使用require语句
+    '@typescript-eslint/no-empty-function': 'off',  // 禁止使用空函数
+    'vue/custom-event-name-casing': 'off',  // 对自定义事件名称强制使用特定的大小写
+    '@typescript-eslint/no-use-before-define': 'off', // 在定义变量之前不允许使用变量
+    '@typescript-eslint/ban-ts-comment': 'off', // 禁止使用@ts-注解
+    '@typescript-eslint/ban-types': 'off', // 禁止使用特定类型
+    '@typescript-eslint/no-non-null-assertion': 'off', // 禁止使用!后缀运算符进行非null断言
+    '@typescript-eslint/explicit-module-boundary-types': 'off', // 在导出的函数和类的公共类方法上需要显式的返回值和参数类型
+    // ↓禁止使用未使用的变量
+    '@typescript-eslint/no-unused-vars': [
+      'error',
+      {
+        argsIgnorePattern: '^h$',
+        varsIgnorePattern: '^h$',
+      },
+    ],
+    // ↓禁止使用未使用的变量
+    'no-unused-vars': [
+      'error',
+      {
+        argsIgnorePattern: '^h$',
+        varsIgnorePattern: '^h$',
+      },
+    ],
+    // ↓强制属性顺序
+    'vue/attributes-order': 'off',
+    // ↓强制每个组件应位于其自己的文件中
+    'vue/one-component-per-file': 'off',
+    // ↓在标签的右括号之前要求或不允许换行
+    'vue/html-closing-bracket-newline': 'off',
+    // ↓强制每行的最大属性数
+    'vue/max-attributes-per-line': 'off',
+    // ↓在多行元素的内容之前和之后需要换行
+    'vue/multiline-html-element-content-newline': 'off',
+    // ↓在单行元素的内容之前和之后需要换行
+    'vue/singleline-html-element-content-newline': 'off',
+    // ↓在模板中的自定义组件上实施属性命名样式
+    'vue/attribute-hyphenation': 'off',
+    // ↓需要道具的默认值
+    'vue/require-default-prop': 'off',
+    // ↓实施自我封闭的风格
+    // 'vue/html-self-closing': 'off',
+    'vue/html-self-closing': [
+      'error',
+      {
+        html: {
+          void: 'always',
+          normal: 'never',
+          component: 'always',
+        },
+        svg: 'always',
+        math: 'always',
+      },
+    ],
 
     // 警告
     "no-extra-boolean-cast": 1, // 不必要的bool转换
