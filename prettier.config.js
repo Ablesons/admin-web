b543204db479e66fba40a8e6fc1335ca4bdf010e
Ablesons <huangzhendong1996@outlook.com>
2022-03-01 17:36:21 +0800

    初始导入

--- prettier.config.js
+++ prettier.config.js
@@ -15,7 +15,7 @@ module.exports = {
   // 在对象，数组括号与文字之间加空格 "{ foo: bar }"
   bracketSpacing: true,
   // 在对象或数组最后一个元素后面是否加逗号（在ES5中加尾逗号）
-  trailingComma: 'none',
+  trailingComma: 'es5',
   // 在jsx中把'>' 是否单独放一行
   jsxBracketSameLine: false,
   // 在jsx中使用单引号代替双引号
