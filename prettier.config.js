abed4a68b9617e0f90a74d19a7a78670dd1075f6
Ablesons <huangzhendong1996@outlook.com>
2022-03-01 16:35:24 +0800

    初始导入

--- prettier.config.js
+++ prettier.config.js
@@ -15,16 +15,18 @@ module.exports = {
   // 在对象，数组括号与文字之间加空格 "{ foo: bar }"
   bracketSpacing: true,
   // 在对象或数组最后一个元素后面是否加逗号（在ES5中加尾逗号）
-  trailingComma: 'es5',
+  trailingComma: 'none',
   // 在jsx中把'>' 是否单独放一行
   jsxBracketSameLine: false,
   // 在jsx中使用单引号代替双引号
   jsxSingleQuote: false,
   // 箭头函数中的括号
   arrowParens: 'avoid',
+  insertPragma: false,
+  requirePragma: false,
   // 默认值。因为使用了一些折行敏感型的渲染器（如GitHub comment）而按照markdown文本样式进行折行
   proseWrap: 'never',
   htmlWhitespaceSensitivity: 'strict',
   // 结尾是 \n \r \n\r auto
-  endOfLine: 'auto',
+  endOfLine: 'lf',
 };
